6be939dbee217cebca69cdadfa12f723835ef943
Toni8482 <toni8482@outlook.com>
2026-08-17 20:14:04 +0200

    rutas y organización de archivos js

---
+++ js/contact.js
@@ -0,0 +1,89 @@
+import { initPreloader } from './modules/preloader.js';
+import { initProgressBar } from './modules/progressBar.js';
+
+import { initReveal } from './modules/reveal.js';
+
+import { initLightbox } from './modules/lightbox.js';
+import { initHeaderShrink } from './modules/headerShrink.js';
+import { initMobileMenu } from './modules/mobileMenu.js';
+
+import { initTheme } from './modules/theme.js';
+import { initMagneticButtons } from './modules/magneticButtons.js';
+
+import { initScrollTop } from './modules/scrollTop.js';
+import { initContactForm } from './modules/contactForm.js';
+
+import { loadComponent } from './modules/loadComponents.js';
+import { safeInit } from './modules/safeInit.js';
+
+
+// Registrar ScrollTrigger si existe
+if (
+  typeof gsap !== 'undefined' &&
+  typeof ScrollTrigger !== 'undefined'
+) {
+  gsap.registerPlugin(ScrollTrigger);
+}
+
+async function initApp() {
+  try {
+    // Cargar componentes comunes
+    const base = document.body.dataset.base || '.';
+
+    await loadComponent(
+      '#header-container',
+      `${base}/components/header.html`
+    );
+
+    await loadComponent(
+      '#footer-container',
+      `${base}/components/footer.html`
+    );
+
+     await loadComponent(
+      '#btn-whatsapp',
+      `${base}/components/btnWhatsApp.html`
+    );
+     await loadComponent(
+      '#btn-scroll-top',
+      `${base}/components/btnScrollTop.html`
+    );
+
+      await loadComponent(
+      '#cta-container',
+      `${base}/components/cta.html`
+    );
+
+   safeInit(initPreloader, 'preloader');
+safeInit(initProgressBar, 'progressBar');
+
+safeInit(initReveal, 'reveal');
+
+safeInit(initLightbox, 'lightbox');
+
+safeInit(initHeaderShrink, 'headerShrink');
+safeInit(initMobileMenu, 'mobileMenu');
+safeInit(initTheme, 'theme');
+
+safeInit(initMagneticButtons, 'magneticButtons');
+
+safeInit(initScrollTop, 'scrollTop');
+
+safeInit(initContactForm, 'contactForm');
+
+
+  } catch (error) {
+    console.error('Error inicializando la aplicación:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+// Refrescar ScrollTrigger cuando todo esté cargado
+
+/** */
+window.addEventListener('load', () => {
+  if (typeof ScrollTrigger !== 'undefined') {
+    ScrollTrigger.refresh();
+  }
+});
